02a48fa7a796b11919f37c389efcb18a864ae19e
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-04-09 02:54:04 +0500

    Initialized

---
+++ src/main/java/com/springbootrest/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.springbootrest.api.v1.mapper;
+
+import com.springbootrest.api.v1.model.CategoryDTO;
+import com.springbootrest.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+
+    @Mapping(source = "id",target = "id")
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
